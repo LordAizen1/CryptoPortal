e0e4d997792ac7affd9893e4341edd684a1e36ad
Mohammad Kaif <mohdkaif2003@gmail.com>
2025-02-07 23:53:30 +0530

    added course details page, scroll to top function

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,25 +1,28 @@
-import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout';
-import Home from './pages/Home';
-import Courses from './pages/Courses';
-import Events from './pages/Events';
-import Members from './pages/Members';
-import About from './pages/About';
-import UserProfile from './pages/UserProfile';
+import React from "react";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import Courses from "./pages/Courses";
+import CourseDetails from "./pages/CourseDetails";
+import Events from "./pages/Events";
+import Members from "./pages/Members";
+import About from "./pages/About";
+import UserProfile from "./pages/UserProfile";
+import ScrollToTop from "./components/ScrollToTop"; // Import ScrollToTop
 
 function App() {
   return (
     <Router>
+      <ScrollToTop /> {/* Add this component */}
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/courses" element={<Courses />} />
+          <Route path="/courses/:courseId" element={<CourseDetails />} />
           <Route path="/events" element={<Events />} />
           <Route path="/members" element={<Members />} />
           <Route path="/about" element={<About />} />
           <Route path="/profile" element={<UserProfile />} />
-          {/* Other routes will be added as we implement more pages */}
         </Routes>
       </Layout>
     </Router>
