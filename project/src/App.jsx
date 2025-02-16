639193cd3843365ac26f3405ffa1918f2c8ac7cc
Mohammad Kaif <mohdkaif2003@gmail.com>
2025-02-16 19:14:48 +0530

    connected blogs and labs pages

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -5,8 +5,11 @@ import Home from "./pages/Home";
 import Courses from "./pages/Courses";
 import CourseDetails from "./pages/CourseDetails";
 import Events from "./pages/Events";
+import Resources from "./pages/Resources";
 import Members from "./pages/Members";
 import About from "./pages/About";
+import Blog from "./pages/Blogs";
+import Labs from "./pages/Labs";
 import UserProfile from "./pages/UserProfile";
 import ScrollToTop from "./components/ScrollToTop"; // Import ScrollToTop
 
@@ -19,10 +22,13 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/courses" element={<Courses />} />
           <Route path="/courses/:courseId" element={<CourseDetails />} />
+          <Route path="/resources" element={<Resources />} />
           <Route path="/events" element={<Events />} />
           <Route path="/members" element={<Members />} />
           <Route path="/about" element={<About />} />
           <Route path="/profile" element={<UserProfile />} />
+          <Route path='/blog' element={<Blog />} />
+          <Route path='/labs' element={<Labs />} />
         </Routes>
       </Layout>
     </Router>
